49bbca3b4bd8db436c047d3140a44c42a22c5fdc
Godwin peter .O <me@godwin.dev>
2020-11-02 20:30:13 +0100

    Allow swap usage with kube

--- docker-kube-setup.sh
+++ docker-kube-setup.sh
@@ -74,11 +74,23 @@ EOF
   Environment="KUBELET_EXTRA_ARGS=--fail-swap-on=false"
 EOF
 
-  echo -ne "y\n" | kubeadm reset 
-  echo 'Environment="KUBELET_EXTRA_ARGS=--fail-swap-on=false"' | sudo tee -a /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+  echo -ne "y\n" | sudo kubeadm reset 
+  sudo sed -i '9s/^/Environment="KUBELET_EXTRA_ARGS=--fail-swap-on=false"\n/' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+
+  sudo echo "
+  kind: MasterConfiguration
+  apiVersion: kubeadm.k8s.io/v1alpha1
+  api:
+    bindPort: ${K8S_API_PORT}
+  apiServerCertSANs: ${K8S_API_EXTRA_HOSTS}
+  " > /opt/kube-setup/config.yaml
+  ## Start cluster initialization
+  #kubeadm init --config /opt/kube-setup/config.yaml --ignore-preflight-errors Swap
+  ## make possible to run workload on master
+  #kubectl taint nodes --all node-role.kubernetes.io/master-
 
   # Allows network bridging.
-  cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+  sudo cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
   net.bridge.bridge-nf-call-ip6tables = 1
   net.bridge.bridge-nf-call-iptables = 1
 EOF

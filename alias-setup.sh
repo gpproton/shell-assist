0ea6329b8c9dec88f85c3fb7e64090f0f3fa574f
radioActive DROID <me@godwin.dev>
2022-07-23 03:34:42 +0100

    Update alias-setup.sh

--- alias-setup.sh
+++ alias-setup.sh
@@ -4,16 +4,14 @@ mkdir -p ~/.bashrc.d/ &&
     cp -r ./alias/*.bashrc ~/.bashrc.d/
 
 ## Add alias to preffered terminal
-# export ALIAS_HELPER_DIR=~/.bashrc.d
-# cat >>~/.bashrc <<EOF
-# #########################################
-# ########## Helper unix alias ############
-# ## General and system command alias
-# [[ -f $ALIAS_HELPER_DIR/general.bashrc ]] && . $ALIAS_HELPER_DIR/general.bashrc
-# [[ -f $ALIAS_HELPER_DIR/system.bashrc ]] && . $ALIAS_HELPER_DIR/system.bashrc
-# ## Docker, docker-compose and docker-stack command alias
-# [[ -f $ALIAS_HELPER_DIR/docker.bashrc ]] && . $ALIAS_HELPER_DIR/docker.bashrc
-# [[ -f $ALIAS_HELPER_DIR/docker-compose.bashrc ]] && . $ALIAS_HELPER_DIR/docker-compose.bashrc
-# [[ -f $ALIAS_HELPER_DIR/docker-stack.bashrc ]] && . $ALIAS_HELPER_DIR/docker-stack.bashrc
-# #########################################
-# EOF
+export ALIAS_HELPER_DIR=~/.bashrc.d
+cat >>~/.bashrc <<EOF
+########## Helper unix alias ############
+## General and system command alias
+[[ -f $ALIAS_HELPER_DIR/general.bashrc ]] && . $ALIAS_HELPER_DIR/general.bashrc
+[[ -f $ALIAS_HELPER_DIR/system.bashrc ]] && . $ALIAS_HELPER_DIR/system.bashrc
+## Docker, docker-compose and docker-stack command alias
+[[ -f $ALIAS_HELPER_DIR/docker.bashrc ]] && . $ALIAS_HELPER_DIR/docker.bashrc
+[[ -f $ALIAS_HELPER_DIR/docker-compose.bashrc ]] && . $ALIAS_HELPER_DIR/docker-compose.bashrc
+[[ -f $ALIAS_HELPER_DIR/docker-stack.bashrc ]] && . $ALIAS_HELPER_DIR/docker-stack.bashrc
+EOF

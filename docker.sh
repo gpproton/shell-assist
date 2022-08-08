43ef21b93bf239b34a1c66471e0c8d6c38b4b4f7
radioActive DROID <me@godwin.dev>
2022-08-08 16:02:28 +0100

    Added debian docker setup scripts

--- docker.sh
+++ docker.sh
@@ -4,13 +4,23 @@ if [ -f "$(dirname $0)/_shared.bashrc" ]; then
     source "$(dirname $0)/_shared.bashrc"
 fi
 
+## Debian remove
 sudo apt-get remove docker docker-engine docker.io containerd runc
 
+## Setup repo and keys for selected OS
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+
 # Install and setup docker
-sudo apt install -y docker.io docker-compose &&
-    sudo systemctl enable docker.service --now &&
-    # Change docker default cgroup driver
-    cat >/etc/docker/daemon.json <<EOF
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose &&
+    sudo systemctl enable docker.service --now
+
+# Change docker default cgroup driver
+cat >/etc/docker/daemon.json <<EOF
 {
     "exec-opts": ["native.cgroupdriver=systemd"],
     "log-driver": "json-file",

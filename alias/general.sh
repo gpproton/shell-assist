5f57098ac2be53e3aa44d2dbc84a3360fdc0129a
Godwin peter .O <me@godwin.dev>
2024-12-27 23:10:42 +0100

    fix: previous path shortcut

--- alias/general.sh
+++ alias/general.sh
@@ -10,16 +10,16 @@ alias l.='ls -d .* --color=auto'
 alias cd..='cd ..'
 ## a quick way to get out of current directory ##
 alias ..='cd ..'
-alias ...='cd ../../../'
-alias ....='cd ../../../../'
-alias .....='cd ../../../../'
+alias ...='cd ../../'
+alias ....='cd ../../../'
+alias .....='cd ../../../'
 alias .4='cd ../../../../'
 alias .5='cd ../../../../..'
 
 ## Create directory and move into it.
 function mcd() {
-    mkdir -p $1
-    cd $1
+  mkdir -p $1
+  cd $1
 }
 ## Colorize the grep command output for ease of use (good for log files)##
 alias grep='grep --color=auto'
@@ -30,14 +30,14 @@ alias fgrep='fgrep --color=auto'
 alias rmf='rm -rf'
 # Clear build files for some dev envs
 function rmdev() {
-    dev_build_dirs=(build bin venv env dist .quasar .output .next .nuxt node_modules .composer)
-    for dir in "${dev_build_dirs[@]}"; do
-        echo "Trying deletion for $dir/*"
-        if (rm -rf ./**/$dir/ &>/dev/null); then
-            echo "Deletion for $dir/* successfully completed"
-        fi
-    done
-    unset dev_build_dirs
+  dev_build_dirs=(build bin venv env dist .quasar .output .next .nuxt node_modules .composer)
+  for dir in "${dev_build_dirs[@]}"; do
+    echo "Trying deletion for $dir/*"
+    if (rm -rf ./**/$dir/ &>/dev/null); then
+      echo "Deletion for $dir/* successfully completed"
+    fi
+  done
+  unset dev_build_dirs
 }
 # do not delete / or prompt if deleting more than 3 files at a time #
 alias rm='rm -I --preserve-root'
@@ -74,13 +74,14 @@ alias ports='netstat -tulanp'
 
 alias wget='wget -c'
 
-export DEV_BOX="$HOME/work/dev-box"
-export SANDBOX="$HOME/work/dev-sandbox"
+export DEV_BOX="$HOME/personal"
+export SANDBOX="$HOME/sandbox"
+
+mkdir -p "$HOME/sandbox/"
+mkdir -p "$HOME/personal/"
+
 alias dev="cd $DEV_BOX"
 alias box="cd $DEV_BOX"
 alias sand="cd $SANDBOX"
 alias sandbox="cd $SANDBOX"
-alias devil="cd $HOME/devilbox"
-alias docs="cd $HOME/work/dev-docs"
-alias rands="cd $HOME/work/dev-randoms"
-alias media="cd $HOME/space/media"
+alias media="cd $HOME/media/"
